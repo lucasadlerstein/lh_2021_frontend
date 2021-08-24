f4e34179da664baabf90b63602d633d4ceb85b8a
lucasadlerstein <lucasadlerstein@gmail.com>
2021-08-24 12:20:03 -0300

    Cambio de fecha meta

--- components/general/Layout.js
+++ components/general/Layout.js
@@ -68,7 +68,7 @@ const Layout = ({children}) => {
                 <link rel="shortcut icon" href="/img/logos/favicon.ico" type="image/x-icon" />
                 <link rel="icon" href="/img/logos/favicon.ico" type="image/x-icon" />
                 <meta property="og:title" content="LATAM Hospitals - Evento Virtual de Salud" />
-                <meta property="og:description" content="Encuentro interdisciplinario entre grandes profesionales y empresas. 23/11 al 2/12." />
+                <meta property="og:description" content="Encuentro interdisciplinario entre grandes profesionales y empresas. 18/10 al 22/10." />
                 <meta property="og:image" content="\img\Latam_Hospitals_2020_Share.jpg" />
                 <meta property="og:url" content="https://latamhospitals.com" />
                 <meta property="og:site_name" content="LATAM HOSPITALS" />
