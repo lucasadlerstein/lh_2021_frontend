1bb4a2e0a3c951331e503e1f703d904b679998c5
lucasadlerstein <lucasadlerstein@gmail.com>
2021-09-10 14:40:29 -0300

    VerAgenda21 Btn

--- components/agenda/Contenido2020.js
+++ components/agenda/Contenido2020.js
@@ -3,6 +3,7 @@ import FranjaContenidoAnterior from './FranjaContenidoAnterior';
 import {Container} from 'reactstrap';
 import {conferenciasEmpresas2020, camarasInstituciones2020, mastertalks2020} from '../../arrEventos2020.js';
 import styled from '@emotion/styled';
+import Link from 'next/link';
 import {withTranslation} from '../../i18n';
  
 const Titulo = styled.h2`
@@ -15,6 +16,13 @@ const Titulo = styled.h2`
 const Contenido2020 = ({t}) => {
     return (
         <div className="py-5r">
+            <div className="text-center pb-5"> 
+                <Link href="/agenda">
+                    <a className="btn-lh btn-prim bor-rad-5 text-center text-uppercase" style={{padding: '1rem 3rem'}}>
+                        {t('VerAgenda2021')}
+                    </a>
+                </Link>
+            </div>
             <Titulo className="text-center">{t('2020.Titulo')}</Titulo>
             
             <FranjaContenidoAnterior eventosMostrar={conferenciasEmpresas2020} titulo={t('Items.ConfEmpresas')} />
