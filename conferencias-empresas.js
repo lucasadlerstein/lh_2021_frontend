ca46e9a14b6bb1e8cc9143007c8d9a12fb8ca1d3
lucasadlerstein <lucasadlerstein@gmail.com>
2021-10-05 16:13:57 -0300

    Conferencias home update

--- conferencias-empresas.js
+++ conferencias-empresas.js
@@ -2,7 +2,24 @@
 
 export const conferenciasEmpresas2021 = [
 
-
+    {
+        "slug": "conferencia/snibe-sars-cov-2",
+        "portadaES": "Conferencia_SNIBE_ES-min.jpg",
+        "portadaEN": "Conferencia_SNIBE_EN-min.jpg",
+        "portadaPR": "Conferencia_SNIBE_PT-min.jpg",
+    },
+    {
+        "slug": "conferencia/conferencia/snibe-experiencia-laboratorio-endocrinologia",
+        "portadaES": "Conferencia_SNIBE_laboratorio_endocrinologia_ES-min.jpg",
+        "portadaEN": "Conferencia_SNIBE_laboratorio_endocrinologia_EN-min.jpg",
+        "portadaPR": "Conferencia_SNIBE_laboratorio_endocrinologia_PT-min.jpg",
+    },
+    {
+        "slug": "conferencia/exo-esoluciones-tecnologicas-para-hospitales",
+        "portadaES": "Conferencia_EXO_Soluciones_Tecnologicas_ES-min.jpg",
+        "portadaEN": "Conferencia_EXO_Soluciones_Tecnologicas_EN-min.jpg",
+        "portadaPR": "Conferencia_EXO_Soluciones_Tecnologicas_PT-min.jpg",
+    },
     {
         "slug": "conferencia/merck-agua-purificada-en-aplicaciones-clinicas-y-de-diagnostico",
         "portadaES": "Conferencia_Merck_Agua_Purificada_Latam_Hospitals_Web_ES-min.jpg",
@@ -27,111 +44,10 @@ export const conferenciasEmpresas2021 = [
         "portadaEN": "Conferencia_Mindray_Morfologia-Celular_Latam_Hospitals_Web_EN-min.jpg",
         "portadaPR": "Conferencia_Mindray_Morfologia-Celular_Latam_Hospitals_Web_PT-min.jpg",
     },
-    
-
-    // {
-    //     "slug": "conferencia/asesoramiento-genetico-oncologico-una-mirada-de-hospital-oncologico-",
-    //     "portadaES": "Conferencia_AsesoramientoGeneticoOncologico_ES-min.jpg",
-    //     "portadaEN": "Conferencia_AsesoramientoGeneticoOncologico_EN-min.jpg",
-    //     "portadaPR": "Conferencia_AsesoramientoGeneticoOncologico_PT-min.jpg",
-    // },
-
-
-    // {
-    //     "slug": "conferencia/el-laboratorio-de-proteinas-junto-a-las-distintas-especialidades-medicas",
-    //     "portadaES": "Conferencia_ForodeProteinas_ES-min.jpg",
-    //     "portadaEN": "Conferencia_ForodeProteinas_EN-min.jpg",
-    //     "portadaPR": "Conferencia_ForodeProteinas_PT-min.jpg",
-    // },
-
-
-    // {
-    //     "slug": "conferencia/covid-19-vacunas-variantes-desafios-y-perspectivas",
-    //     "portadaES": "Conferencia_VacunasCovid19_ES-min.jpg",
-    //     "portadaEN": "Conferencia_VacunasCovid19_EN-min.jpg",
-    //     "portadaPR": "Conferencia_VacunasCovid19_PT-min.jpg",
-    // },
-
-
-    // {
-    //     "slug": "mastertalk/neuroinflamacion-el-principal-culpable-de-la-enfermedad-de-alzheimer",
-    //     "portadaES": "Conferencia_Neuroinflamaciones_ES-min.jpg",
-    //     "portadaEN": "Conferencia_Neuroinflamaciones_EN-min.jpg",
-    //     "portadaPR": "Conferencia_Neuroinflamaciones_PT-min.jpg",
-    // },
-
-    // {
-    //     "slug": "mastertalk/cannabis-medicinal",
-    //     "portadaES": "Conferencia_CannabisMedicinal_ES-min.jpg",
-    //     "portadaEN": "Conferencia_CannabisMedicinal_EN-min.jpg",
-    //     "portadaPR": "Conferencia_CannabisMedicinal_PT-min.jpg",
-    // },
-
-    // {
-    //     "slug": "conferencia/accreditation-canada-atencion-centrada-en-las-personas-en-la-era-pandemica",
-    //     "portadaES": "Conferencia_Atencionpersonalizada_ES-min.jpg",
-    //     "portadaEN": "Conferencia_Atencionpersonalizada_EN-min.jpg",
-    //     "portadaPR": "Conferencia_Atencionpersonalizada_PT-min.jpg",
-    // },
-
-    // {
-    //     "slug": "mastertalk/marcadores-serologicos-en-posvacunacion-covid-19",
-    //     "portadaES": "Conferencia_MarcadoresSerologicos_ES-min.jpg",
-    //     "portadaEN": "Conferencia_MarcadoresSerologicos_EN-min.jpg",
-    //     "portadaPR": "Conferencia_MarcadoresSerologicos_PT-min.jpg",
-    // },
-
-    // {
-    //     "slug": "conferencia/aptm-trayectoria-evolucion-desarrollo-1992-2021",
-    //     "portadaES": "Conferencia_APTM_ES-min.jpg",
-    //     "portadaEN": "Conferencia_APTM_EN-min.jpg",
-    //     "portadaPR": "Conferencia_APTM_PT-min.jpg",
-    // },
-
-    // {
-    //     "slug": "conferencia/avances-de-la-salud-digital-en-centroamerica",
-    //     "portadaES": "Conferencia_AvancesDeLaSaludDigital_ES-min.jpg",
-    //     "portadaEN": "Conferencia_AvancesDeLaSaludDigital_EN-min.jpg",
-    //     "portadaPR": "Conferencia_AvancesDeLaSaludDigital_PT-min.jpg",
-    // },
-
-    // {
-    //     "slug": "conferencia/covid-y-diabetes-desafios-desde-la-sociedad-argentina-de-diabetes",
-    //     "portadaES": "Conferencia_CovidyDiabetes_ES-min.jpg",
-    //     "portadaEN": "Conferencia_CovidyDiabetes_EN-min.jpg",
-    //     "portadaPR": "Conferencia_CovidyDiabetes_PT-min.jpg",
-    // },
-
-    // {
-    //     "slug": "mastertalk/perfiles-hormonales-en-las-distintas-etapas-de-la-vida-diagnostico-prenatal-pesquisa-neonatal-infancia-etapa-reproductiva-menopausia",
-    //     "portadaES": "Conferencia_Perfileshormonales_ES-min.jpg",
-    //     "portadaEN": "Conferencia_Perfileshormonales_EN-min.jpg",
-    //     "portadaPR": "Conferencia_Perfileshormonales_PT-min.jpg",
-    // },
-
-    // {
-    //     "slug": "mastertalk/actualizacion-en-informe-del-colesterol-ldl-en-el-estudio-de-lipidos",
-    //     "portadaES": "Conferencia_ActualizacionenInformedelColesterol_ES-min.jpg",
-    //     "portadaEN": "Conferencia_ActualizacionenInformedelColesterol_EN-min.jpg",
-    //     "portadaPR": "Conferencia_ActualizacionenInformedelColesterol_PT-min.jpg",
-    // },
-
-    // {
-    //     "slug": "conferencia/importancia-de-la-microbiologia-molecular-en-el-control-de-infecciones-asociadas-a-la-atencion-de-salud-y-uso-racional-de-antibioticos",
-    //     "portadaES": "Conferencia_Importanciadelamicrobiologia_ES-min.jpg",
-    //     "portadaEN": "Conferencia_Importanciadelamicrobiologia_EN-min.jpg",
-    //     "portadaPR": "Conferencia_Importanciadelamicrobiologia_PT-min.jpg",
-    // },
-
-    // {
-    //     "slug": "mastertalk/transformacion-digital-para-la-inclusion",
-    //     "portadaES": "Conferencia_Transformaciondigitalparalainclusion_ES-min.jpg",
-    //     "portadaEN": "Conferencia_Transformaciondigitalparalainclusion_EN-min.jpg",
-    //     "portadaPR": "Conferencia_Transformaciondigitalparalainclusion_PT-min.jpg",
-    // },
-
-
-
-
-
+    {
+        "slug": "conferencia/bg-analizadores-alifax-nefelometria-laser",
+        "portadaES": "Conferencia_BG_ALIFAX_ES-min.jpg",
+        "portadaEN": "Conferencia_BG_ALIFAX_EN-min.jpg",
+        "portadaPR": "Conferencia_BG_ALIFAX_PT-min.jpg",
+    },
 ]
